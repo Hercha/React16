37d0d58aef86ea6c517361aefe560a62d38364a0
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-24 00:03:27 +0100

    challenge-2 solution

--- challange-2/src/App.js
+++ challange-2/src/App.js
@@ -1,21 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
+  state = {
+    userInput: ''
+  }
+
+  inputChangedHandler = (event) => {
+    this.setState({userInput: event.target.value});
+  }
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
+  }
+
   render() {
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char
+       character={ch}
+      key={index}
+      clicked={() => this.deleteCharHandler(index)} />;  
+      });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+          type="text"
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput} />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
 }
 
 export default App;
+
+
+// class App extends Component {
+//   state = {
+//     userInput: '',
+//     textLength: null
+//   }
+
+//   inputChangedHandler = (event) => {
+//     this.setState({userInput: event.target.value});
+//     this.setState({textLength: this.state.userInput.length});
+//   }
+
+//   render() {
+//     console.log(this.state.textLength);
+//     return (
+//       <div className="App">
+//         <input type="text"
+//           onChange={this.inputChangedHandler}
+//           value={this.state.userInput} />
+//         <p>{this.state.textLength}</p>
+//       </div>
+//     );
+//   }
+// }
