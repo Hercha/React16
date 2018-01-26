48ce78df30b30ff4689497a8edf7b96ac52838cd
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-26 23:44:01 +0100

    Sidedrawer, responsive adjustments

---
+++ burger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Logo from '../../Layout/Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+
+const sideDrawer = (props) => {
+    //....
+    return (
+        <div className={classes.SideDrawer}>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            {/* <Logo height="11%"/> */}
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
+};
+
+export default sideDrawer;
