31006995143d49473a2bfaa176defd389c566cf1
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-27 01:06:47 +0100

    Sidedrawer toggle button

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -7,24 +7,30 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
 
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
     render () {
         return (
             <Aux>
-            <Toolbar />
-            <SideDrawer
-                open={this.state.showSideDrawer}
-                closed={this.sideDrawerClosedHandler} />
-            <main className={classes.Content}>
-                {this.props.children}
-            </main>
-        </Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
         )
     }
 }
