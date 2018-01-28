75be18697a342f3a889f07f87e2c9cf653fdabd6
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-29 00:17:30 +0100

    Axios success handling

--- burger/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger/src/hoc/withErrorHandler/withErrorHandler.js
@@ -6,21 +6,43 @@ import Aux from '../Auxiliary/Auxiliary';
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
         state = {
-            error: null
+            error: null,
+            success: null
         }
 
-        componentDidMount () {
+        // componentDidMount () {
+        //     axios.interceptors.request.use(req => {
+        //         this.setState({error: null});
+        //         return req;
+        //     });
+        //     axios.interceptors.response.use(res => res, error => {
+        //         this.setState({error: error});
+        //     });
+        // }
+
+        /** ALTERNATIV WAY WITH SUCCESS MESSAGE */
+        componentWillMount () {
             axios.interceptors.request.use(req => {
-                this.setState({error: null});
-                return req;
+              this.setState({ error: null, success: null });
+              return req;
             });
-            axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
+       
+            axios.interceptors.response.use(res => {
+              if(res.config.method !== 'get') {
+                this.setState({ success: 'Your request is already sent' });
+              }
+              return res;
+            }, error => {
+              this.setState({ error: error });
             });
-        }
+          }
 
         errorConfirmedHandler = () => {
             this.setState({error: null})
+        }
+
+        successConfirmedHandler = () => {
+            this.setState({success: null})
         }
 
         render () {
@@ -30,6 +52,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
                      show={this.state.error}
                      modalClosed={this.errorConfirmedHandler}>
                         {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    {/* ALTERNATIVE WAY SHOWING SUCCESS MESSAGE*/}
+                    <Modal show={this.state.success}
+                            modalClosed={this.successConfirmedHandler} >
+                            {this.state.success ? this.state.success : null}
                     </Modal>
                     <WrappedComponent {...this.props} />
                 </Aux>
