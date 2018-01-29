b757cb6c6b99144145f2b9d0eae1a9671fa2e778
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-29 13:13:02 +0100

    Navigation programmatically

--- http-server-ajax/src/containers/Blog/Posts/Posts.js
+++ http-server-ajax/src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -30,8 +30,12 @@ class Posts extends Component {
             });
     }
 
+    // postSelectedHandler = (id) => {
+    //     this.setState({selectedPostId: id});
+    // }
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.props.history.push({pathname: '/' + id});
+        // this.props.history.push('/' + id);
     }
 
     render () {
@@ -39,14 +43,22 @@ class Posts extends Component {
         if(!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                <Link to={'/' + post.id} key={post.id}>
-                    <Post
-                        title={post.title} 
-                        author={post.author}
-                        // {...this.props}
-                        //match={this.props.match}
+                // <Link to={'/' + post.id} key={post.id}>
+                //     <Post
+                //         title={post.title} 
+                //         author={post.author}
+                //         // {...this.props}
+                //         //match={this.props.match}
+                //     clicked={() => this.postSelectedHandler(post.id)} />
+                // </Link>);
+                <Post
+                    key={post.id}
+                    title={post.title} 
+                    author={post.author}
+                    // {...this.props}
+                    //match={this.props.match}
                     clicked={() => this.postSelectedHandler(post.id)} />
-                </Link>);
+                );
             });
         }
 
