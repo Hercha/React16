4d8bd0e559a2cff60620990c407317720a94dc56
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-29 09:04:17 +0100

    setting up router package, preparing project for routing

--- http-server-ajax/src/containers/Blog/Blog.js
+++ http-server-ajax/src/containers/Blog/Blog.js
@@ -1,60 +1,11 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        error: false,
-        selectedPostId: null 
-        //selectedPost: null
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
-    /** ALT WAY, CHANGE TO ALT IN FullPost.js TOO */
-    // postSelectedHandler = (id) => {
-    //     let postToPass = this.state.posts.filter(post => post.id === id);
-    //     this.setState({selectedPost: postToPass});
-    //     }
-
-    componentDidMount () {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }   
-                });
-                this.setState({posts: updatedPosts});
-                //console.log(response);
-            })
-            .catch(error => {
-                //console.log(error);
-                this.setState({error: true});
-            });
-    }
 
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
-        if(!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
 
         return (
             <div className="Blog">
@@ -66,16 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                    {/* <FullPost selectedPost={this.state.selectedPost}/> */}
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
     }
