33c67c4b2de574bb861d39f44ca225a996664917
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-29 14:53:39 +0100

    routing and server deployment

--- http-server-ajax/src/App.js
+++ http-server-ajax/src/App.js
@@ -6,6 +6,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="/my-app"> //if using app in a subdirectory on a live server, 
+      //server also needs to forward even if it dosent know the pages location
       <BrowserRouter>
         <div className="App">
           <Blog />
