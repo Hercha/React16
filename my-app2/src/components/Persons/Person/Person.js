ef6e467a7b013ee11deab19f52dbf178c316f214
Hercha <jens.hedberg@medieinstitutet.se>
2019-03-24 15:15:25 +0100

    Rendering adjacent JSX elements wit react fragments instead of Auxiliari HOC component

--- my-app2/src/components/Persons/Person/Person.js
+++ my-app2/src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
-import Auxiliary from '../../../hoc/Auxiliary';
+//import Auxiliary from '../../../hoc/Auxiliary';
 //import classes from './Person.module.css';
 
 class Person extends Component {
@@ -12,11 +12,11 @@ class Person extends Component {
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.name} />
         </div> */
-        <Auxiliary>
+        <Fragment>
             <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.name} />
-        </Auxiliary>
+        </Fragment>
     );
   }
 };
