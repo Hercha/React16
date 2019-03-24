c35259c05f128b7cbc5edad8f991d055323aef55
Hercha <jens.hedberg@medieinstitutet.se>
2019-03-24 20:42:39 +0100

    contextType & useContext

--- my-app2/src/components/Cockpit/Cockpit.js
+++ my-app2/src/components/Cockpit/Cockpit.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './Cockpit.module.css';
 import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
 
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
@@ -51,9 +54,7 @@ const cockpit = (props) => {
         className={btnClass}
         onClick={props.clicked}>Toggle Persons
       </button>
-      <AuthContext.Consumer>
-        {context => <button onClick={context.login}>Log in</button>}
-      </AuthContext.Consumer>
+      <button onClick={authContext.login}>Log in</button>
     </div>
   );
 
--- my-app2/src/components/Persons/Person/Person.js
+++ my-app2/src/components/Persons/Person/Person.js
@@ -12,9 +12,12 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     //this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
@@ -26,9 +29,7 @@ class Person extends Component {
             <input type="text" onChange={this.props.changed} value={this.props.name} />
         </div> */
         <Auxiliary>
-            <AuthContext.Consumer>
-              {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
-            </AuthContext.Consumer>
+              {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
             <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p>{this.props.children}</p>
             <input 
