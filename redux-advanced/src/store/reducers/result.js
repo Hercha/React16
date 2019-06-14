d7256709db55c9f2a8c7e610501b6337b00adee7
Hercha <jens.hedberg@medieinstitutet.se>
2019-06-14 17:26:38 +0200

    Where to put data transforming logic

--- redux-advanced/src/store/reducers/result.js
+++ redux-advanced/src/store/reducers/result.js
@@ -7,9 +7,11 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch ( action.type ) {
         case actionTypes.STORE_RESULT:
+            // Change data
             return {
                 ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
+                results: state.results.concat({id: new Date(), value: action.result })
+                //results: state.results.concat({id: new Date(), value: action.result * 2})
             }
         case actionTypes.DELETE_RESULT:
         const updatedArray = state.results.filter(result => result.id !== action.resultElId);
