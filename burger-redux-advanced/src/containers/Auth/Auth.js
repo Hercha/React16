b4d25b886d80a8dae81e7509cce139f4f71ad77f
Hercha <jens.hedberg@medieinstitutet.se>
2019-06-16 12:02:48 +0200

    Added signin

--- burger-redux-advanced/src/containers/Auth/Auth.js
+++ burger-redux-advanced/src/containers/Auth/Auth.js
@@ -37,7 +37,8 @@ class Auth extends Component {
       valid: false,
       touched: false
     }
-  }
+  },
+  isSignup: true
 }
 
 checkValidity(value, rules) {
@@ -85,7 +86,13 @@ checkValidity(value, rules) {
 
   submitHandler = (event) => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+  }
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return {isSignup: !prevState.isSignup};
+    });
   }
 
   render () {
@@ -115,6 +122,9 @@ checkValidity(value, rules) {
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
+        <Button
+          clicked={this.switchAuthModeHandler} 
+          btnType="Danger">SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}</Button>
       </div>
     );
   }
@@ -122,7 +132,7 @@ checkValidity(value, rules) {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password))
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
   };
 };
 
