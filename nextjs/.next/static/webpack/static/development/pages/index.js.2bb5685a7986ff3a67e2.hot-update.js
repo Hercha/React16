8cc142771e4bbf5eab9af1b808629d484d35d969
Hercha <jen-sa@hotmail.com>
2019-07-11 11:27:06 +0200

    Start of Next.js section

---
+++ nextjs/.next/static/webpack/static/development/pages/index.js.2bb5685a7986ff3a67e2.hot-update.js
@@ -0,0 +1,38 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "E:\\udemycourses\\react16\\React16\\nextjs\\pages\\index.js";
+
+
+var indexPage = function indexPage() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "The Main Page"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (indexPage);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.2bb5685a7986ff3a67e2.hot-update.js.map
