c69ea16a2d3abe146c1a7d76ad581c7fe2366589
Hercha <jen-sa@hotmail.com>
2019-07-14 11:34:59 +0200

    Using the transition component

--- animations/src/App.js
+++ animations/src/App.js
@@ -47,11 +47,17 @@ class App extends Component {
           )}
           </Transition>
          { /*         ></div> : null} */}
+         <Transition
+          mountOnEnter
+          unmountOnExit
+          in={this.state.modalIsOpen}
+          timeout={300}>
+          {state => (
+            <Modal show={state} closed={this.closeModal}/> 
+          )}
+         </Transition>
         {this.state.modalIsOpen ? (
-           <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> 
-          ) : null}
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} /> 
+          <Backdrop show /> 
           ) : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
