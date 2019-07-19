802c1dd36fb768b06948d52b1867cffa10a85db3
Hercha <jen-sa@hotmail.com>
2019-07-19 16:25:35 +0200

    Handling Authentication with a Saga

--- burger-redux-saga/src/store/sagas/auth.js
+++ burger-redux-saga/src/store/sagas/auth.js
@@ -1,5 +1,6 @@
 import { delay } from 'redux-saga/effects';
 import { put } from 'redux-saga/effects';
+import axios from 'axios';
 
 import * as actions from '../actions/index';
 
@@ -11,6 +12,31 @@ export function* logoutSaga(action) {
 }
 
 export function* checkAuthTimeoutSaga(action) {
-  yield delay(action.expirationTime);
+  yield delay(action.expirationTime * 1000);
   yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+  yield put(actions.authStart());
+  const authData = {
+    email: action.email,
+    password: action.password,
+    returnSecureToken: true
+  };
+  let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=[API_KEY]';
+  if (!action.isSignup) {
+    url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=[API-KEY]';
+  }
+  try {
+  const response = yield axios.post(url, authData)
+  
+  const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000);
+  yield localStorage.setItem('token', response.data.idToken);
+  yield localStorage.setItem('expirationDate', expirationDate);
+  yield localStorage.setItem('userId', response.data.localId);
+  yield put(action.authSuccess(response.data.idToken, response.data.localId));
+  yield put(action.checkAuthTimeout(response.data.expiresIn));
+  } catch (error) {
+    yield put(actions.authFail(error.response.data.error));
+  }
 }
